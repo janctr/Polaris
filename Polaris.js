6c6ca97ba6208ae6195793c79c939cde312e6a8c
Jan Iverson Eligio <janiverson.eligio.ctr@us.navy.mil>
2024-07-18 13:30:16 -1000

    Add loaders.

--- Polaris.js
+++ Polaris.js
@@ -186,6 +186,9 @@ class Polaris {
                 appObject.elementId
             );
 
+            // Add loader svg when loading
+            $(`#${appObject.elementId}`).append(loaderEl());
+
             app.getObject(appObject.elementId, appObject.objectId, {
                 noInteraction: false,
             });
@@ -212,6 +215,9 @@ class Polaris {
                 appObject.elementId
             );
 
+            // Add loader svg when loading
+            $(`#${appObject.elementId}`).append(loaderEl());
+
             app.getObject(appObject.elementId, appObject.objectId, {
                 noInteraction: false,
             });
@@ -228,6 +234,9 @@ class Polaris {
         const app = this.qlik.openApp(appId, config);
 
         appObjects.forEach((appObject) => {
+            // Add loader svg when loading
+            $(`#${appObject.elementId}`).append(loaderEl());
+
             app.getObject(appObject.elementId, appObject.objectId, {
                 noInteraction: false,
             });
@@ -479,5 +488,24 @@ const notionalAppObjects = {
             objectId: 'mKw',
         },
     ],
-    cop: [{ elementId: 'map', objectId: 'GcZB' }],
+    cop: [{ elementId: 'map', objectId: 'aDEUH' }],
 };
+
+function loaderEl() {
+    return $(`
+        <div class="loader-wrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 44 44" stroke="blue">
+                <g fill="none" fill-rule="evenodd" stroke-width="2">
+                    <circle cx="22" cy="22" r="1">
+                        <animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"></animate>
+                        <animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"></animate>
+                    </circle>
+                    <circle cx="22" cy="22" r="1">
+                        <animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"></animate>
+                        <animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"></animate>
+                    </circle>
+                </g>
+            </svg>
+        </div>
+    `);
+}
