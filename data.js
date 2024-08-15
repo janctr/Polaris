08428832171e1ed7a47e8227c2928c6e9997f063
Jan Iverson Eligio <janiverson.eligio.ctr@us.navy.mil>
2024-08-14 16:39:01 -1000

    Move data to different file.

---
+++ data.js
@@ -0,0 +1,449 @@
+const angularLinks = [
+    {
+        href: '#/',
+        label: 'POLARIS',
+        classNames: ['polaris-logo', 'no-underline'],
+        anchorClassNames: ['polaris'],
+    },
+    { href: 'Polaris.html', label: 'Home', classNames: ['link'] },
+    {
+        href: '#/log-functions',
+        label: 'Log Functions',
+        classNames: ['link'],
+    },
+    {
+        href: '#/classes-of-supply',
+        label: 'Classes of Supply',
+        classNames: ['link'],
+        childLinks: [
+            {
+                href: '#',
+                label: 'Class I',
+                classNames: ['child-link'],
+                anchorClassNames: [],
+            },
+            {
+                href: '#',
+                label: 'Class III',
+                classNames: ['child-link'],
+                anchorClassNames: [],
+            },
+            {
+                href: '#',
+                label: 'Class IV',
+                classNames: ['child-link'],
+                anchorClassNames: [],
+            },
+            {
+                href: '#',
+                label: 'Class V',
+                classNames: ['child-link'],
+                anchorClassNames: [],
+            },
+            {
+                href: '#',
+                label: 'Class VIII',
+                classNames: ['child-link'],
+                anchorClassNames: [],
+            },
+            {
+                href: '#',
+                label: 'Class IX',
+                classNames: ['child-link'],
+                anchorClassNames: [],
+            },
+        ],
+    },
+    // { href: 'cop.html', label: 'COP', classNames: ['link'] },
+    // {
+    //     href: '',
+    //     label: 'J4 Landing Page',
+    //     classNames: ['link'],
+    //     anchorClassNames: ['j4-landing-page-link'],
+    // },
+];
+
+const logFunctionsPage = {
+    objectElements: [
+        {
+            classes: ['supply'],
+            title: 'Supply',
+            bodyId: 'QV1',
+            // sources: ['USTRANSCOM Global Distribution Network'],
+            // refreshRate: { label: 'Monthly', color: 'yellow' },
+            // quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: ['logistics-nodes'],
+            title: 'Logistics Nodes',
+            bodyId: 'QV2',
+            sources: ['USTRANSCOM Global Distribution Network'],
+            refreshRate: { label: 'Monthly', color: 'yellow' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: ['readiness-airframes'],
+            title: 'PDDOC',
+            bodyId: 'QV3',
+            sources: ['8th TSC', 'PACFLT0'],
+            refreshRate: { label: 'Hourly', color: 'green' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: ['readiness-airframes'],
+            title: 'Readiness - Airframes',
+            bodyId: 'QV4',
+            sources: ['AMSRR', 'G081', 'IMDS'],
+            refreshRate: { label: '~Daily', color: 'green' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: ['engineering'],
+            title: 'Engineering',
+            bodyId: 'QV5',
+            sources: ['Theater Infrastructure Master Plant'],
+            refreshRate: { label: 'Bimonthly', color: 'green' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: ['logistics-services'],
+            title: 'Logistics Services',
+            bodyId: 'QV6',
+        },
+        {
+            classes: ['joint-health-services'],
+            title: 'Joint Health Services',
+            bodyId: 'QV7',
+            sources: ['CarePoint'],
+            refreshRate: { label: 'Static', color: 'red' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: ['ocs'],
+            title: 'OCS',
+            bodyId: 'QV8',
+            sources: ['SPOT-ES'],
+            refreshRate: { label: 'Static', color: 'red' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+    ],
+};
+
+const classesOfSupplyPage = {
+    objectElements: [
+        {
+            classes: ['class-i'],
+            title: 'Class I',
+            bodyId: 'QV1',
+            sources: ['DLA EBS', 'GCSS-A'],
+            refreshRate: { label: '~Daily', color: 'green' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: ['class-iii'],
+            title: 'Class III',
+            bodyId: 'QV2',
+            sources: ['DLA-EBS', 'IAWT'],
+            refreshRate: { label: '~Daily', color: 'green' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: [' class-iv'],
+            title: 'Class IV',
+            bodyId: 'QV3',
+            sources: ['DLA EBS', 'GCSS-A', 'GCSS-MC', 'LMP', 'Navy ERP'],
+            refreshRate: { label: '~Daily', color: 'green' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+        {
+            classes: ['class-v'],
+            title: 'Class V',
+            bodyId: 'QV4',
+            sources: ['AESIP', 'DAAS', 'LMP', 'OIS', 'SAAS', 'TICIMS'],
+            refreshRate: { label: '~Daily', color: 'green' },
+            quality: { label: 'Good', color: 'green' },
+        },
+        {
+            classes: ['class-viii'],
+            title: 'Class VIII',
+            bodyId: 'QV5',
+            sources: ['MEDCOP', 'CarePoint'],
+            refreshRate: { label: 'N/A', color: '' },
+            quality: { label: 'N/A', color: '' },
+        },
+        {
+            classes: ['class-ix'],
+            title: 'Class IX',
+            bodyId: 'QV6',
+            sources: ['DLA EBS', 'GCSS-A', 'GCSS-MC', 'LMP', 'Navy ERP'],
+            refreshRate: { label: '~Daily', color: 'green' },
+            quality: { label: 'Partial', color: 'yellow' },
+        },
+    ],
+};
+
+const siprObjects = {
+    home: [
+        { elementId: 'map', objectId: '7ea8e67c-3241-42ee-8250-de8cadfc303a' },
+        {
+            // Class I
+            label: 'Class I',
+            elementId: 'QV1',
+            objectId: 'ACjABHm',
+        },
+        {
+            // Class III
+            label: 'Class III',
+
+            elementId: 'QV2',
+            objectId: 'jXhfrmR',
+        },
+        {
+            // Class IV
+            label: 'Class IV',
+            elementId: 'QV3',
+            objectId: 'uvcy',
+        },
+        {
+            // Class V
+            label: 'Class V',
+            elementId: 'QV4',
+            objectId: 'pKJXJtS',
+        },
+        {
+            // Class VIII
+            label: 'Class VIII',
+            elementId: 'QV5',
+            objectId: 'qvPEkB',
+        },
+        {
+            // Class IX
+            label: 'Class IX',
+            elementId: 'QV6',
+            objectId: 'WPytt',
+        },
+    ],
+    logFunctions: [
+        {
+            // Supply
+            label: 'Supply',
+            elementId: 'QV1',
+            objectId: 'mENkwjC',
+        },
+        {
+            // Logistics Nodes
+            label: 'Logistics Nodes',
+            elementId: 'QV2',
+            objectId: 'dndvmjp',
+        },
+        {
+            // PDDOC
+            label: 'PDDOC',
+            elementId: 'QV3',
+            objectId: 'tJAutGe',
+        },
+        {
+            // Readiness Airframes
+            label: 'Readiness - Airframes',
+            elementId: 'QV4',
+            objectId: 'bWLGCT',
+        },
+        {
+            // Engineering
+            label: 'Engineering',
+            elementId: 'QV5',
+            objectId: 'jYtzRw',
+        },
+        {
+            // Logistics Services
+            label: 'Logistics Services',
+            elementId: 'QV6',
+            objectId: 'Vby',
+        },
+        {
+            // Joint Health Services
+            label: 'Joint Health Services',
+            elementId: 'QV7',
+            objectId: 'dgkMzt',
+        },
+        {
+            // OCS
+            label: 'OCS',
+            elementId: 'QV8',
+            objectId: 'nPeqdTV',
+        },
+    ],
+    classesOfSupply: [
+        {
+            // Class I
+            label: 'Class I',
+            elementId: 'QV1',
+            objectId: 'ACjABHm',
+        },
+        {
+            // Class III
+            label: 'Class III',
+
+            elementId: 'QV2',
+            objectId: 'jXhfrmR',
+        },
+        {
+            // Class IV
+            label: 'Class IV',
+            elementId: 'QV3',
+            objectId: 'uvcy',
+        },
+        {
+            // Class V
+            label: 'Class V',
+            elementId: 'QV4',
+            objectId: 'pKJXJtS',
+        },
+        {
+            // Class VIII
+            label: 'Class VIII',
+            elementId: 'QV5',
+            objectId: 'qvPEkB',
+        },
+        {
+            // Class IX
+            label: 'Class IX',
+            elementId: 'QV6',
+            objectId: 'WPytt',
+        },
+    ],
+    cop: [
+        { elementId: 'map', objectId: 'cf0bfaac-b1b2-416a-bc58-f2612a8d1f17' },
+    ],
+    template: [
+        { elementId: 'map', objectId: '7ea8e67c-3241-42ee-8250-de8cadfc303a' },
+        {
+            // Class I
+            label: 'Class I',
+            elementId: 'QV1',
+            objectId: 'ACjABHm',
+        },
+        {
+            // Class III
+            label: 'Class III',
+
+            elementId: 'QV2',
+            objectId: 'jXhfrmR',
+        },
+        {
+            // Class IV
+            label: 'Class IV',
+            elementId: 'QV3',
+            objectId: 'uvcy',
+        },
+        {
+            // Class V
+            label: 'Class V',
+            elementId: 'QV4',
+            objectId: 'pKJXJtS',
+        },
+        {
+            // Class VIII
+            label: 'Class VIII',
+            elementId: 'QV5',
+            objectId: 'qvPEkB',
+        },
+        {
+            // Class IX
+            label: 'Class IX',
+            elementId: 'QV6',
+            objectId: 'WPytt',
+        },
+    ],
+};
+
+const notionalAppObjects = {
+    logFunctions: [
+        {
+            label: 'Supply',
+            elementId: 'QV1',
+            objectId: 'GcZB',
+        },
+        {
+            label: 'Logistics Nodes',
+            elementId: 'QV2',
+            objectId: 'frXbuh',
+        },
+        { label: 'PDDOC', elementId: 'QV3', objectId: 'fsHmHP' },
+        { label: 'Readiness - Airframes', elementId: 'QV4', objectId: 'mKw' },
+        { label: 'Engineering', elementId: 'QV5', objectId: 'YJgJM' },
+        { label: 'Logistics Services', elementId: 'QV6', objectId: 'mKw' },
+        {
+            label: 'Joint Health Services',
+            elementId: 'QV7',
+            objectId: 'fsHmHP',
+        },
+        { label: 'OCS', elementId: 'QV8', objectId: 'frXbuh' },
+    ],
+    classesOfSupply: [
+        {
+            label: 'Class I',
+            elementId: 'QV1',
+            objectId: 'GcZB',
+        },
+        {
+            label: 'Class III',
+            elementId: 'QV2',
+            objectId: 'frXbuh',
+        },
+        {
+            label: 'Class IV',
+            elementId: 'QV3',
+            objectId: 'fsHmHP',
+        },
+        {
+            label: 'Class V',
+            elementId: 'QV4',
+            objectId: 'mKw',
+        },
+        {
+            label: 'Class VIII',
+            elementId: 'QV5',
+            objectId: 'YJgJM',
+        },
+        {
+            label: 'Class IX',
+            elementId: 'QV6',
+            objectId: 'UNDffs',
+        },
+    ],
+    cop: [{ elementId: 'map', objectId: 'aDEUH' }],
+    template: [
+        { elementId: 'map', objectId: 'aDEUH' },
+        {
+            label: 'Class I',
+            elementId: 'QV1',
+            objectId: 'GcZB',
+        },
+        {
+            label: 'Class III',
+            elementId: 'QV2',
+            objectId: 'frXbuh',
+        },
+        {
+            label: 'Class IV',
+            elementId: 'QV3',
+            objectId: 'fsHmHP',
+        },
+        {
+            label: 'Class V',
+            elementId: 'QV4',
+            objectId: 'mKw',
+        },
+        {
+            label: 'Class VIII',
+            elementId: 'QV5',
+            objectId: 'YJgJM',
+        },
+        {
+            label: 'Class IX',
+            elementId: 'QV6',
+            objectId: 'UNDffs',
+        },
+    ],
+};
