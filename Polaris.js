d351d12d3e75e58ee1f3a668e19c14f78b785f29
Jan Iverson Eligio <janiverson.eligio.ctr@us.navy.mil>
2024-09-12 12:54:22 -1000

    Add properties close icon component. Try different layout for tile pages.

--- Polaris.js
+++ Polaris.js
@@ -892,8 +892,17 @@ require(['js/qlik'], function (qlik) {
     angularApp.component('closeIcon', {
         template: closeIconTemplate,
         bindings: {
-            toggleMenu: '&',
+            handleClick: '&',
+            size: '@',
+            color: '@',
+            fillColor: '@',
         },
+        controller: [
+            '$scope',
+            function ($scope) {
+                $scope.getFillColor = (fillColor) => fillColor || '#000000';
+            },
+        ],
     });
 
     angularApp.component('fullscreenIcon', {
@@ -946,8 +955,8 @@ const burgerMenuIconTemplate = `
 `;
 
 const closeIconTemplate = `
-<div class="close-icon" ng-click="$ctrl.toggleMenu()">
-    <svg fill="#000000" height="100%" width="100%" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+<div class="close-icon" ng-click="$ctrl.handleClick()">
+    <svg fill="{{ getFillColor($ctrl.fillColor) }}" height="100%" width="100%" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
         <g>
             <g>
